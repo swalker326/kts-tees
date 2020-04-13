52a2aa4bed1f3f0b968aa8e5da08db3683dc15c8
Shane Walker <shane@oak.works>
2020-04-13 16:37:28 -0400

    Updates

---
+++ precache-manifest.e721a67bccc9e9e6bf5668db6ed7c6dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf740a69101df9d21bea5a8605d6febe",
+    "url": "/kts-tees/index.html"
+  },
+  {
+    "revision": "5ccd863c5a28c69b338f",
+    "url": "/kts-tees/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4466a777760c7af909ef",
+    "url": "/kts-tees/static/js/2.5e3afeb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kts-tees/static/js/2.5e3afeb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ccd863c5a28c69b338f",
+    "url": "/kts-tees/static/js/main.e27aaa92.chunk.js"
+  },
+  {
+    "revision": "e0116cfdbd537d3e11a3",
+    "url": "/kts-tees/static/js/runtime-main.182612de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kts-tees/static/media/logo.5d5d9eef.svg"
+  }
+]);
